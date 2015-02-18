43902acf1d82e0676247fcfa940cf249cbf29002
Justin D'Arcangelo <justindarc@gmail.com>
2015-02-18 16:33:05 -0500

    Hard-coded the 'media' gaia-theme into a scoped style for the entire customizer so it is not dependent upon the app's styles.

--- app/js/views/main.js
+++ app/js/views/main.js
@@ -1,7 +1,111 @@
 /* global View */
 
 var mainViewTemplate =
-`<fxos-customizer></fxos-customizer>
+`<style scoped>
+  .fxos-customizer-main-view {
+    font-size: 14px;
+
+    /** Grey Colors
+     ---------------------------------------------------------*/
+
+    --color-alpha: #333333;
+    --color-beta: #ffffff;
+    --color-gamma: #4d4d4d;
+    --color-delta: #5f5f5f;
+    --color-epsilon: #858585;
+    --color-zeta: #a6a6a6;
+    --color-eta: #c7c7c7;
+    --color-theta: #e7e7e7;
+    --color-iota: #f4f4f4;
+
+  /** Brand Colors
+   ---------------------------------------------------------*/
+
+    --color-darkblue: #00539f;
+    --color-blue: #00caf2;
+    --color-turquoise: #27c8c2;
+    --color-darkorange: #e66000;
+    --color-orange: #ff9500;
+    --color-yellow: #ffcb00;
+    --color-violet: #c40c84;
+
+    --color-warning: #fbbd3c;
+    --color-destructive: #e2443a;
+    --color-preffered: #00ba91;
+
+    /** Background
+     ---------------------------------------------------------*/
+
+    --background: var(--color-alpha);
+    --background-plus: var(--color-gamma);
+    --background-minus: #2B2B2B;
+    --background-minus-minus: #1a1a1a;
+
+    /** Borders
+     ---------------------------------------------------------*/
+
+    --border-color: var(--color-gamma);
+
+    /** Highlight Color
+     ---------------------------------------------------------*/
+
+    --highlight-color: var(--color-blue);
+
+    /** Text Color
+     ---------------------------------------------------------*/
+
+    --text-color: var(--color-beta);
+    --text-color-minus: var(--color-eta);
+
+    /** Button
+     ---------------------------------------------------------*/
+
+    --button-background: var(--background-plus);
+
+    /** Links
+     ---------------------------------------------------------*/
+
+    --link-color: var(--highlight-color);
+
+    /** Inputs
+     ---------------------------------------------------------*/
+
+    --input-background: var(--background-plus);
+    --input-color: var(--color-alpha);
+    --input-clear-background: #909ca7;
+
+    /** Buttons
+     ---------------------------------------------------------*/
+
+     --button-box-shadow: none;
+     --button-box-shadow-active: none;
+
+    /** Header
+     ---------------------------------------------------------*/
+
+    --header-background: var(--background);
+    --header-icon-color: var(--text-color);
+    --header-button-color: var(--highlight-color);
+    --header-disabled-button-color: rgba(255,255,255,0.3);
+
+    /** Text Input
+     ---------------------------------------------------------*/
+
+    --text-input-background: var(--background-minus);
+
+    /** Switch
+     ---------------------------------------------------------*/
+
+    --switch-head-border-color: var(--background-minus-minus);
+    --switch-background: var(--background-minus-minus);
+
+    /** Checkbox
+     ---------------------------------------------------------*/
+
+    --checkbox-border-color: var(--background-minus-minus);
+  }
+</style>
+<fxos-customizer></fxos-customizer>
 <fxos-customizer-highlighter></fxos-customizer-highlighter>`;
 
 export default class MainView extends View {
