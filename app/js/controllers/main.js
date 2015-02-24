87c6c8afabae9c780d45292212721981ad61f0d2
Doug Sherk <github@sherk.me>
2015-02-24 16:06:33 -0500

    Bug 1136273 - Pad content body by the height of the Customizer

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -25,6 +25,20 @@ export default class MainController extends Controller {
     this.view.customizer.setRootNode(document.documentElement);
     document.body.appendChild(this.view.el);
 
+    var padding = this.view.el.querySelector('fxos-customizer').offsetHeight;
+    this._styleEl = document.createElement('style');
+    this._styleEl.textContent = `
+    html, body {
+      overflow: initial !important;
+    }
+
+    body::after {
+      content: "";
+      height: ${padding}px;
+      display: block;
+    }`;
+    document.head.appendChild(this._styleEl);
+
     this.attached = true;
   }
 
@@ -34,6 +48,8 @@ export default class MainController extends Controller {
     }
 
     document.body.removeChild(this.view.el);
+    document.head.removeChild(this._styleEl);
+    this._styleEl = null;
 
     this.attached = false;
   }
