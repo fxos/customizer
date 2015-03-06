5977533b743b67f2fedf4d9534a8f21b53a6c1d7
David Flanagan <dflanagan@mozilla.com>
2015-03-05 16:03:30 -0800

    Bugs 1139916 and 1139920: fix two highlighting issues
    fix

--- app/js/elements/fxos-customizer-highlighter.js
+++ app/js/elements/fxos-customizer-highlighter.js
@@ -6,6 +6,7 @@ var shadowHTML =
 .overlay {
   display: none;
   position: absolute;
+  box-sizing: border-box;
   pointer-events: none;
   z-index: 9999999; /* above the app, but below the other customizer elements */
   background-color: #00caf2;
@@ -28,17 +29,23 @@ proto.highlight = function(element) {
   // Figure out where the element is
   var rect = element.getBoundingClientRect();
 
-  // Highlight it
-  // Note that we use add the document scroll offsets to the element
-  // coordinates to get document coordinates instead of screen coordinates.
-  this.overlay.style.left   = (rect.left + window.pageXOffset) + 'px';
-  this.overlay.style.top    = (rect.top + window.pageYOffset) + 'px';
-  this.overlay.style.width  = rect.width + 'px';
-  this.overlay.style.height = rect.height + 'px';
-  this.overlay.style.display = 'block';
+  // If the element has zero size, hide the highlight
+  if (rect.width === 0 && rect.height === 0) {
+    this.overlay.style.display = 'none';
+  }
+  else {
+    // Otherwise, highlight the element.
+    // Note that we use add the document scroll offsets to the element
+    // coordinates to get document coordinates instead of screen coordinates.
+    this.overlay.style.left    = (rect.left + window.pageXOffset) + 'px';
+    this.overlay.style.top     = (rect.top + window.pageYOffset) + 'px';
+    this.overlay.style.width   = rect.width + 'px';
+    this.overlay.style.height  = rect.height + 'px';
+    this.overlay.style.display = 'block';
 
-  // And now make it visible
-  element.scrollIntoView({behavior: 'smooth'});
+    // And try to move the element so that it is on screen
+    element.scrollIntoView({behavior: 'smooth'});
+  }
 };
 
 proto.hide = function() {
