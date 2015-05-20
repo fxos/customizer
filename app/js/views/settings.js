4fd12775b1c3dfced366f980eeb86bc9724b7fac
Justin D'Arcangelo <justindarc@gmail.com>
2015-05-20 18:21:07 -0400

    Bug 1144843 - [Customizer] Wire up addon detail view to Addon Manager activity

--- app/js/views/settings.js
+++ app/js/views/settings.js
@@ -3,14 +3,11 @@
 var settingsViewTemplate =
 `<gaia-modal class="settings">
   <gaia-header>
-    <button data-action="close">Close</button>
+    <button data-action="close" data-icon="close"></button>
     <h1>Settings</h1>
-    <button data-action="addons">All Add-ons</button>
+    <button data-action="toggleMerging">Merge</button>
   </gaia-header>
-  <section>
-    <gaia-sub-header>Installed Add-ons</gaia-sub-header>
-    <gaia-list></gaia-list>
-  </section>
+  <gaia-list></gaia-list>
 </gaia-modal>`;
 
 export default class SettingsView extends View {
@@ -26,72 +23,63 @@ export default class SettingsView extends View {
     super(controller);
 
     this.modal  = this.$('gaia-modal');
-    this.addons = this.$('gaia-list');
+    this.list = this.$('gaia-list');
+    this.mergeButton = this.$('button[data-action="toggleMerging"]');
 
-    this.on('click', 'gaia-button', this._handleClick.bind(this));
-    this.on('click', 'button', this._handleClick.bind(this));
+    this.on('click', 'button', e => this._handleButtonClick(e));
+    this.on('click', 'gaia-button', e => this._handleButtonClick(e));
 
-    // this.on('change', 'gaia-switch', this._handleChange.bind(this));
+    this.list.addEventListener('click', e => this._handleListClick(e));
   }
 
   template() {
     return settingsViewTemplate;
   }
 
-  setAddons(addons) {
-    this.addons.innerHTML = '';
+  setMerging(merging) {
+    if (merging) {
+      this.mergeButton.textContent = 'Done';
+    }
 
-    addons.forEach((addon) => {
-      var installTime = new Date(addon.installTime);
-      this.addons.innerHTML +=
-`<li flexbox>
-  <span flex>
-    <gaia-switch data-origin="${addon.origin}" data-enabled="${addon.enabled}"></gaia-switch>
-  </span>
-  <span flex>
-    ${addon.manifest.name}
-    <span class="addon-time">
-      ${installTime.toLocaleDateString()}
-      ${installTime.toLocaleTimeString()}
-    </span>
-  </span>
-  <span flex>
-    <gaia-button circular data-action="uninstall" data-origin="${addon.origin}">
-      <i data-icon="delete"></i>
-    </gaia-button>
-  </span>
-</li>`;
-    });
-
-    [].forEach.call(this.addons.querySelectorAll('gaia-switch'), (gs) => {
-      if (gs.dataset.enabled === 'true') {
-        gs.setAttribute('checked', true);
-      }
-
-      delete gs.dataset.enabled;
-    });
-
-    // XXX - Shouldn't have to do this, but 'change' is not propagating
-    [].forEach.call(document.querySelectorAll('gaia-switch'), (gs) => {
-      gs.addEventListener('change', this._handleChange.bind(this));
-    });
+    else {
+      this.mergeButton.textContent = 'Merge';
+    }
   }
 
-  _handleClick(evt) {
+  setAddons(addons) {
+    this.list.innerHTML = '';
+
+    addons.forEach((addon) => {
+      this.list.innerHTML +=
+`<a data-manifest-url="${addon.manifestURL}">
+  <div flex>
+    <h3>${addon.manifest.name}</h3>
+    <p>${addon.enabled ? 'Enabled' : 'Disabled'}</p>
+  </div>
+  <i data-icon="forward-light"></i>
+</a>`;
+    });
+
+    this.list.innerHTML +=
+`<div>
+  <gaia-button flex data-action="addonManager">Open Add-on Manager</gaia-button>
+</div>`;
+  }
+
+  _handleButtonClick(evt) {
     var action = this.controller[evt.target.dataset.action];
     if (typeof action === 'function') {
       action.call(this.controller, evt.target.dataset);
     }
   }
 
-  _handleChange(evt) {
-    var gaiaSwitch = evt.target;
-    if (gaiaSwitch.checked) {
-      this.controller.enableAddon(gaiaSwitch.dataset);
+  _handleListClick(evt) {
+    var target = evt.target.closest('a');
+    if (!target) {
+      return;
     }
 
-    else {
-      this.controller.disableAddon(gaiaSwitch.dataset);
-    }
+    var manifestURL = target.dataset.manifestUrl;
+    this.controller.showAddon(manifestURL);
   }
 }
